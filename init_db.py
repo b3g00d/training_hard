0aa40576666376b962d6e8e9ab88a2bc3a3f1e68
mr2uang <mr2uang55@gmail.com>
2016-12-13 10:45:12 +0700

    them default accout

--- init_db.py
+++ init_db.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-from my_db import db
+from my_db import db, User, Album
 
 if __name__ == '__main__':
     db.create_all()
+    db.session.add(User('admin', 'admin@gmail.com'))
+    db.session.commit()
